2f0463bf2fd689c18055825d5fd31d1b47754e1c
nisse <nielsvaes@gmail.com>
2021-01-08 18:23:25 +0100

    marking it as alpha for version 1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tangle-node-editor',
-    version='0.0.1',
+    version='1.0.0-alpha',
     packages=['tangle'],
     url='https://www.github.com/nielsvaes/tangle',
     long_description=long_description,
