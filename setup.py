1a89827be27069509ffa8436e07be5dec60d2f43
nisse <nielsvaes@gmail.com>
2021-01-10 00:23:41 +0100

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tangle-node-editor',
-    version='1.0.4-alpha',
+    version='1.0.5-alpha',
     packages=find_packages(),
     url='https://www.github.com/nielsvaes/tangle',
     long_description=long_description,
