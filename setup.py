602a7dcc888d36af0fe74de4b56ea8c9671296ca
nisse <nielsvaes@gmail.com>
2021-01-10 12:22:27 +0100

    including data files

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# any data files that match this pattern will be include in the build
+data_files_to_include = ["*.png", "*.tngl", "*.jpg"]
+
 setup(
     name='tangle-node-editor',
-    version='1.0.5-alpha',
+    version='1.0.6-alpha',
     packages=find_packages(),
+    package_data={
+        "": data_files_to_include,
+    },
     url='https://www.github.com/nielsvaes/tangle',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,6 +23,6 @@ setup(
     description='A Python based node editor',
 
     entry_points = {
-                   'console_scripts': ['tangle=tangle.app:main'],
-               },
+        'console_scripts': ['tangle=tangle.app:main'],
+    },
 )
